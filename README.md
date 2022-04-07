5afa2d30a280b19eaac2e52af7bc9666ae7d4b20
Chulpyo Kim <kim.chulpyo@gmail.com>
2022-04-07 15:35:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Algorithm
+알고리즘 학습 프로젝트
+
+# Requirements
+# How to use
---
+++ src/app/programmers/tempalte.py
@@ -0,0 +1,10 @@
+"""programmers python code template
+"""
+
+def solution():
+    pass
+
+if __name__ == '__main__':
+    args = ()
+    for arg in args:
+        print(solution(*arg))
