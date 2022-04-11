d1c5d5f4e78355a7cc5108110a692ccbcfeb1fd3
Chulpyo Kim <kim.chulpyo@gmail.com>
2022-04-11 15:56:36 +0900

    77484 템플릿 및 예제 설정

---
+++ src/app/programmers/lvl1/77484/__init__.py
@@ -0,0 +1,18 @@
+"""로또의 최고 순위와 최저 순위
+"""
+
+args = (
+    (([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]), [3, 5] )
+    , (([0, 0, 0, 0, 0, 0],	[38, 19, 20, 40, 15, 25]), [1, 6])
+    , (([45, 4, 35, 20, 3, 9]	[20, 9, 3, 45, 4, 35]), [1, 1])
+)
+
+def solution(lottos, win_nums):
+    answer = []
+    return answer
+
+
+if __name__ == '__main__':
+    for arg in args:
+        result = solution(*arg[0])
+        print(f'answer is {arg[1]}. your answer is {result}. {"O" if arg[1] == result else "X"}')
