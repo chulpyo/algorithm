c05f6d014428b7aca20cd023ba85aebbed16d3a4
Chulpyo Kim <kim.chulpyo@gmail.com>
2022-04-11 16:06:07 +0900

    77484 문제 풀이 완료

--- src/app/programmers/lvl1/77484/__init__.py
+++ src/app/programmers/lvl1/77484/__init__.py
@@ -4,12 +4,16 @@
 args = (
     (([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]), [3, 5] )
     , (([0, 0, 0, 0, 0, 0],	[38, 19, 20, 40, 15, 25]), [1, 6])
-    , (([45, 4, 35, 20, 3, 9]	[20, 9, 3, 45, 4, 35]), [1, 1])
+    , (([45, 4, 35, 20, 3, 9],	[20, 9, 3, 45, 4, 35]), [1, 1])
 )
 
+RANK = [6, 6, 5, 4, 3, 2, 1]
+
 def solution(lottos, win_nums):
-    answer = []
-    return answer
+    known = sum( lotto > 0 and lotto in win_nums for lotto in lottos )
+    unknown = sum( lotto == 0 for lotto in lottos)
+
+    return [RANK[known+unknown], RANK[known]]
 
 
 if __name__ == '__main__':
